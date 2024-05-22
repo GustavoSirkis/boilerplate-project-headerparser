5852a02d9a37739fd58a5c1f544ddcdb74043339
GustavoSirkis <gustavosirkis@gmail.com>
2024-05-22 08:58:33 -0300

    Microsserviço conversor de requisição de cabeçalho

--- index.js
+++ index.js
@@ -20,8 +20,10 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami', (req, res) => {
+  const ip = req.ip;
+
+  res.json({ ipaddress: ip, language: req.headers['accept-language'], software: req.headers['user-agent']});
 });
 
 // listen for requests :)
